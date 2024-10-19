b0344273b497f61cae4876eba4df452eb8105ae1
Cristian Romanesei <cromanesei@gmail.com>
2024-10-19 06:16:36 +0000

    update test 4

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -18,13 +18,13 @@ suite('Unit Tests', function () {
     test('#isOk, #isNotOk', function () {
       assert.isNotOk(null, 'null is falsey');
       assert.isOk("I'm truthy", 'A string is truthy');
-      assert.isOk(true, 'true is truthy');
+      assert.isNotOk(true, 'true is truthy');
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
       assert.isTrue(true, 'true is true');
       assert.isTrue(!!'double negation', 'Double negation of a truthy value is true');
-      assert.isFalse({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+      assert.isTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
   });
 
